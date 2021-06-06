0089c9211f07ef47c49214765daf95590a326180
Jason Dinh <dinhjdev@gmail.com>
2021-06-06 17:19:00 -0500

    First Java Program,

---
+++ Java/Java Fundamentals/First Java Program/Introduction.java
@@ -0,0 +1,5 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("My name is Jason Dinh\nI am 23 years old\nMy hometown is Dallas, TX");
+    }
+}
