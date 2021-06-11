28711be2e740c499232262816e9db18f07955625
Jason Dinh <dinhjdev@gmail.com>
2021-06-10 23:24:34 -0500

    BasicJava

---
+++ Java/Java Fundamentals/Basics/BasicJava.java
@@ -0,0 +1,19 @@
+public class BasicJava {
+    public void print1To255() {
+        for (int i = 1; i <= 255; i++){
+            System.out.println(i);
+        }
+    }
+
+    public void printOddTo255() {
+        for (int i = 1; i <= 255; i++){
+            if (i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public void printSum() {
+        
+    }
+}
---
+++ Java/Java Fundamentals/Basics/BasicJavaTest.java
@@ -0,0 +1,8 @@
+public class BasicJavaTest {
+    public static void main(String[] args) {
+        BasicJava basic = new BasicJava();
+
+        basic.print1To255();
+        basic.printOddTo255();
+    }
+}
