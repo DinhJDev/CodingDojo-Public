4d3717fabcff24a4f7a6ded9da7850876dd444b6
Jason Dinh <dinhjdev@gmail.com>
2021-06-14 01:08:07 -0500

    update: List of Exceptions

--- Java/Java Fundamentals/List of Exceptions/ExceptionsList.java
+++ Java/Java Fundamentals/List of Exceptions/ExceptionsList.java
@@ -1,13 +1,5 @@
 import java.util.*;
 
-class TestException extends Exception{}
-class TestProgram {
-    public boolean testTry() throws TestException{
-        System.out.println("Index: " + i + " Value: " + myList.get(i));
-        throw new TestException();
-    }
-}
-
 public class ExceptionsList {
     public static void main(String[] args) {
         ArrayList<Object> myList = new ArrayList<Object>();
@@ -15,13 +7,13 @@ public class ExceptionsList {
         myList.add("hello world");
         myList.add(48);
         myList.add("Goodbye World");
-        try {
             for(int i = 0; i < myList.size(); i++) {
-                Integer castedValue = (Integer) myList.get(i);
+                try {
+                    Integer castedValue = (Integer) myList.get(i);
+                } catch (Exception ex) {
+                    System.out.println("Index: " + i + " Value: " + myList.get(i));
+                }
             }
-        } catch (TestException e) {
-
-        }
     }
 }
 
