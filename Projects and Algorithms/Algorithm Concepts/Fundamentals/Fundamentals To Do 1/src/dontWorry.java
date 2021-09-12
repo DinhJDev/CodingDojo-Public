38fcb7f615c697020036db14a2d3b0a3cb138112
Jason Dinh <dinhjdev@gmail.com>
2021-09-12 10:31:24 -0500

    Fundamentals To Do 1

---
+++ Projects and Algorithms/Algorithm Concepts/Fundamentals/Fundamentals To Do 1/src/dontWorry.java
@@ -0,0 +1,10 @@
+public class dontWorry {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 98; i++) {
+            beCheerful();
+        }
+    }
+    static void beCheerful() {
+        System.out.println("good morning!");
+    }
+}
---
+++ Projects and Algorithms/Algorithm Concepts/Fundamentals/Fundamentals To Do 1/src/printFrom.java
@@ -0,0 +1,7 @@
+public class printFrom {
+    public static void main(String[] args) {
+        for (int i = -52; i <= 1066 ; i++ ) {
+            System.out.println(i);
+        }
+    }
+}
