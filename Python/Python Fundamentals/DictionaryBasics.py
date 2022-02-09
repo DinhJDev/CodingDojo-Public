2721f6dce86ac43ed7d235ba1452be4aa4170f81
Jason Dinh <dinhjdev@gmail.com>
2022-02-08 23:54:05 -0600

    Dictionary Basics

---
+++ Python/Python Fundamentals/DictionaryBasics.py
@@ -0,0 +1,6 @@
+x = {"name":"Jason","age":"24","country":"The United States","language":"Python"}
+
+print("My name is " + x["name"])
+print("My age is " + x["age"])
+print("My country of birth is " + x["country"])
+print("My favorite language is " + x["language"])
